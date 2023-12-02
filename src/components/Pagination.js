935b112938a036de4a6afa75ad372418a3db98a2
Harsh Raj <78961911+Harshraj77@users.noreply.github.com>
2023-12-02 14:11:34 +0530

    pagination css update

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,14 +2,42 @@ import React from 'react';
 
 const Pagination = ({ currentPage, totalPages, handlePageChange }) => {
   return (
-    <div>
-      <button className="first-page" onClick={() => handlePageChange(1)}>First</button>
-      <button className="previous-page" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+    <div className="flex items-center justify-center space-x-2 py-4">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => handlePageChange(1)}
+      >
+        First
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </button>
       {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
-        <button key={page} onClick={() => handlePageChange(page)} className={page === currentPage ? 'active' : ''}>{page}</button>
+        <button
+          key={page}
+          onClick={() => handlePageChange(page)}
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${page === currentPage ? 'bg-blue-700' : ''}`}
+        >
+          {page}
+        </button>
       ))}
-      <button className="next-page" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
-      <button className="last-page" onClick={() => handlePageChange(totalPages)}>Last</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => handlePageChange(totalPages)}
+      >
+        Last
+      </button>
     </div>
   );
 };
